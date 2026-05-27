2df268260690a402b4f7ff851024f5b6cd3fc3ae
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2026-05-27 19:37:38 -0300

    adicionando requisitos normais e interface básica

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@
 #define MAX 10
 
 typedef enum {
-  PAID,
-  LATE
+  PAID = 1,
+  LATE = 2,
 } Status;
 
 typedef struct {
@@ -45,6 +45,7 @@ User creat_new_user(char *name, Status status) {
 
 void register_new_user(User user) {
   if (db->size >= MAX) {
+    printf("Banco de dados cheio\n");
     return;
   }
 
@@ -52,21 +53,148 @@ void register_new_user(User user) {
   db->size += 1;
 }
 
+void show_users() {
+  if (db->size <= 0) {
+    printf("Banco de dados vazio\n");
+    return;
+  }
+  for(int i = 0; i < db->size; i++) {
+    printf("Id: %d, Nome: %s, Situação Financeira: %s\n", 
+        db->users[i].id, db->users[i].name, db->users[i].status == PAID ? "Paid" : "Late");
+  }
+}
+
+void get_user_by_id(int id) {
+  if (db->size <= 0) {
+    printf("Banco de dados vazio\n");
+    return;
+  }
+
+  for(int i = 0; i < db->size; i++) {
+    if (db->users[i].id == id) {
+      printf("Id: %d, Nome: %s, Situação Financeira: %s\n", 
+        db->users[i].id, db->users[i].name,  db->users[i].status == PAID ? "Paid" : "Late");
+      return;
+    }
+  }
+
+  printf("Nao encontrado\n");
+}
+
+void remove_user_by_id(int id) {
+  if (db->size <= 0) {
+    printf("Banco de dados vazio\n");
+    return;
+  }
+
+  for(int i = 0; i < db->size; i++) {
+    if (db->users[i].id == id) {
+      db->users[i] = db->users[db->size - 1];
+      db->users[db->size - 1] = (User){0, "", PAID};
+      db->size -= 1;
+      return;
+    }
+  }
+
+  printf("Nao encontrado\n");
+}
+
+int validadate_input() {
+  char input[255];
+  char *temp;
+  int valid_input;
+  scanf("%s", input);
+  
+  valid_input = strtol(input, &temp, 10);
+
+  if(*temp != '\0') {
+    printf("Entrada invalida\n");
+    return -1;
+  }
+
+  return valid_input;
+}
+
+void interface_new_user() {
+  char name[255];
+  Status status;
+
+  printf("Nome: ");
+  scanf("%s", name);
+
+  printf("Status\n1 - Paid\n2 - Late\n> ");
+  status = validadate_input();
+  printf("%d", status);
+
+  if (status != PAID && status != LATE) {
+    printf("Opcao invalida\n");
+    interface_new_user();
+    return;
+  }
+
+  User user = creat_new_user(name, status);
+  register_new_user(user);
+}
+
+void interface() {
+  int run = 1;
+  printf("Bem vindo ao Sistema de Sintuação Cadastral\n");
+
+  while(run) {
+    printf("\n------------------------------\n");
+    printf("\nSelecione uma opção\n\n");
+    printf("1 - Cadastrar novo usuario\n");
+    printf("2 - Listar todos os usuarios\n");
+    printf("3 - Buscar usuario por id\n");
+    printf("4 - Remover usuario por id\n");
+    printf("5 - Sair\n\n> ");
+    
+    int option = validadate_input(), id;
+    char name[255];
+    
+    switch(option) {
+      case 1:
+        printf("\033[2J\033[1H");
+        interface_new_user();
+      break;
+      
+      case 2:
+        printf("\033[2J\033[1H");
+        show_users();
+      break;
+      
+      case 3:
+        printf("\033[9A\033[0J");
+        printf("Digite o id: ");
+        id = validadate_input();
+        get_user_by_id(id);
+      break;
+      
+      case 4:
+        printf("\033[9A\033[0J");
+        printf("Digite o id: ");
+        id = validadate_input();
+        remove_user_by_id(id);
+      break;
+      
+      case 5:
+        run = 0;
+      break;
+
+      default:
+        printf("\033[2J\033[1H");
+        printf("Opcao invalida\n");
+      break;
+    }
+  }
+}
 
 int main(void) {
   srand(time(NULL));
 
   open_database();
 
-  User user = creat_new_user("Wallace", PAID);
-  register_new_user(user);
-  user = creat_new_user("Felipe", LATE);
-  register_new_user(user);
-
-  for(int i = 0; i < db->size; i++) {
-    printf("Id: %d, Nome: %s, Situação Financeira: %s\n", 
-        db->users[i].id, db->users[i].name, !db->users[i].status ? "Paid" : "Late");
-  }
+  interface();
   
   close_database();
 }
