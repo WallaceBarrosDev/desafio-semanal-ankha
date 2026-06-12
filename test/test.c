f5f50205613ca274bf442989da33eb201223e433
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2026-06-12 19:06:43 -0300

    fix

--- test/test.c
+++ test/test.c
@@ -9,8 +9,8 @@ int main(void) {
     cmocka_unit_test_setup(testInvalidName, setupClient),
     cmocka_unit_test_setup(testNullName, setupClient),
     cmocka_unit_test_setup(testRegistrationSize8, setupClient),
-    cmocka_unit_test_setup(testSaveValidClient, setupClient),
-    cmocka_unit_test_setup(testSaveNullClient, setupClient)
+    cmocka_unit_test_setup(testSaveValidClient, setupAPI),
+    cmocka_unit_test_setup(testSaveNullClient, setupAPI)
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
