2c5c2b70489918e31eca5f067e512cf4c8e57d74
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2026-06-13 10:00:34 -0300

    feat(api) implementado as regras de negocios de registro de usuário

--- src/api/saveNewClient.c
+++ src/api/saveNewClient.c
@@ -1,5 +1,22 @@
 #include "saveNewClient.h"
 
+// TODO: implementar as fetures dos testes;
 void saveNewClient(Client *client) {
+// TODO: verificar se a struct Client não e null
+  if(client == NULL) {
+    throwException(EXCEPTION_ARGUMENT_IS_NULL, "Client is null");
+    return;
+  }
+// TODO: verificar se o nome e null
+  if(client->name == NULL) {
+    throwException(EXCEPTION_INVALID_NAME, "invalid name");
+    return;
+  }
+// TODO: verificar se a matricula e valida
+  if (client->registration < 10000000 || client->registration > 99999999) {
+    throwException(EXCEPTION_INVALID_REGISTRATION, "invalid registration");
+    return;
+  }
 
+  writeClientToFile(client);
 }
--- src/api/saveNewClient.h
+++ src/api/saveNewClient.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include "../domain/client.h"
+#include "../repository/writeClientToFile.h"
 #include "../exceptions/exceptions.h"
 
 void saveNewClient(Client *client);
